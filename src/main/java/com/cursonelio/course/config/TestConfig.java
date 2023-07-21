145947b14d06e3089ebc0caf44c259f8d7381d9f
Kirmct de Abreu <kirmctneto@gmail.com>
2023-07-21 10:49:20 -0400

    Entity category

--- src/main/java/com/cursonelio/course/config/TestConfig.java
+++ src/main/java/com/cursonelio/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.cursonelio.course.config;
 
+import com.cursonelio.course.entities.Category;
 import com.cursonelio.course.entities.Order;
 import com.cursonelio.course.entities.User;
 import com.cursonelio.course.entities.enums.OrderStatus;
+import com.cursonelio.course.repositories.CategoryRepository;
 import com.cursonelio.course.repositories.OrderRepository;
 import com.cursonelio.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +23,16 @@ public class TestConfig implements CommandLineRunner {//tudo sera executado quan
     private UserRepository userRepository; //injetando dependencia implicito/automatico
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -32,5 +42,6 @@ public class TestConfig implements CommandLineRunner {//tudo sera executado quan
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
     }
 }
